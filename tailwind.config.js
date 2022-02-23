6f18ea2d3b1eaf31c8f8f61d58b37e1ac7a448b8
st4l1nR <61132529+st4l1nR@users.noreply.github.com>
2022-02-22 22:43:04 -0500

    tailwindcss configured

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
